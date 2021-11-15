268e8611e316b0324ae66a208e16e553471a104c
Dans-sys <danielsolano712@gmail.com>
2021-11-15 15:50:21 -0600

    Toma de fotografia y ubicacion

---
+++ PM2E2GRUPO7/Views/MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Plugin.Geolocator;
+using PM2E2GRUPO7.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Reflection;
+using System.IO;
+using Plugin.Media;
+
+namespace PM2E2GRUPO7.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public bool takedfoto = false;
+        public byte[] imgbyte;
+        public Image image = new Image();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Ubicacion();
+            TakePhoto.Clicked += TakePhoto_Clicked;
+        }
+
+        // INICIO UBICACION
+        private async void Ubicacion()
+        {
+            if (!CrossGeolocator.IsSupported)
+            {
+                await DisplayAlert("Error", "Ha ocurrido un error al cargar el plugin", "OK");
+                return;
+            }
+
+            CrossGeolocator.Current.PositionChanged += Current_PositionChanged;
+
+            await CrossGeolocator.Current.StartListeningAsync(new TimeSpan(0, 0, 1), 0.5);
+        }
+
+        private void Current_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            if (!CrossGeolocator.Current.IsListening)
+            {
+
+                return;
+            }
+            var position = CrossGeolocator.Current.GetPositionAsync();
+
+            txtlatitud.Text = position.Result.Latitude.ToString();
+            txtlongitud.Text = position.Result.Longitude.ToString();
+        }
+
+        private async Task ValidationForm()
+        {
+            if (String.IsNullOrWhiteSpace(txtlatitud.Text) || 
+                String.IsNullOrWhiteSpace(txtlongitud.Text) || 
+                String.IsNullOrWhiteSpace(txtdescripcion.Text) ||
+                takedfoto == false)
+            {
+                await this.DisplayAlert("Advertencia", "Todos los campos son obligatorios", "OK");
+            }
+        }
+
+        private async void Toolbar01_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void Toolbar02_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new OTRA_PANTALLA());
+        }
+
+        private async void BtnGuardar_Clicked(object sender, EventArgs e)
+
+        {
+            if (ValidationForm().IsCompleted)
+            {
+                ClearScreen();
+                await DisplayAlert("Salvado", "Guardado Exitosamente", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Error", "No se pudo guardar la ubicacion", "Ok");
+            }
+        }
+
+        private void ClearScreen()
+        {
+            this.txtlatitud.Text = String.Empty;
+            this.txtlongitud.Text = String.Empty;
+            this.txtdescripcion.Text = String.Empty;
+            Photo.Source = ImageSource.FromFile("noimagen.png");
+            takedfoto = false;
+        }
+
+        private async void BtnUbicacion_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new OTRA_PANTALLA());
+        }
+        // FIN UBICACION
+
+        // INICIO CAMARA
+        private async void TakePhoto_Clicked(object sender, EventArgs e)
+        {
+            var photo =
+               await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions());
+            if (photo != null)
+            {
+                Photo.Source = ImageSource.FromStream(() =>
+                {
+
+                    takedfoto = true;
+                    image.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+                    using (MemoryStream memory = new MemoryStream())
+                    {
+
+                        Stream stream = photo.GetStream();
+                        stream.CopyTo(memory);
+                        imgbyte = memory.ToArray();
+                    }
+                    return photo.GetStream();
+                });
+            }
+            else
+            {
+                await DisplayAlert("Error", "No hay foto", "OK");
+            }
+        }
+        // FIN CAMARA
+    }
+}
